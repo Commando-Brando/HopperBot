6842c34f61f39b20f972adea41f90cf725ed8912
Brandon Muller <60548867+Commando-Brando@users.noreply.github.com>
2022-05-02 21:43:17 -0500

    Added Algos to be used

--- commands/Utility.py
+++ commands/Utility.py
@@ -1,7 +1,19 @@
 from discord.ext import commands
 
 
+
 class Utility(commands.Cog):
+    Algo = (
+        ("MergeSort", "Merge.JPG"),
+        ("BubbleSort",
+         "https://adrianmejia.com/most-popular-algorithms-time-complexity-every-programmer-should-know-free-online"
+         "-tutorial-course/#Bubble-sort"),
+        ("QuickSort", "https://www.geeksforgeeks.org/quick-sort/"),
+        ("HeapSort", "https://www.geeksforgeeks.org/heap-sort/"),
+        ("SelectionSort", "https://www.geeksforgeeks.org/selection-sort/"),
+        ("RadixSort", "https://www.geeksforgeeks.org/radix-sort/"),
+        ("BucketSort", "https://www.geeksforgeeks.org/bucket-sort-2/"))
+
     def __init__(self, bot):
         self.bot = bot
 
